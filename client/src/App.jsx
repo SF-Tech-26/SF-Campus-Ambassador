df36f1d83dfefefc86cf4af1d7eccae6649bfcac
Gandharv-999fx <imkumargandharv@gmail.com>
2025-11-08 16:34:33 +0530

    Homepage final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,40 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import FormPage from "./pages/FormPage";
 import HomePage from "./pages/HomePage";
-import Testimonials from './components/testimonials2'
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Testimonials from './components/testimonials2';
+
+
 
 export default function App() {
   return (
-    <Router>
-      {/* <nav className="absolute top-0 w-full flex justify-between p-4 text-white z-20 font-semibold">
-        <Link to="/" className="hover:text-cyan-400 transition">Home</Link>
-        <Link to="/form" className="hover:text-purple-400 transition">Form</Link>
-      </nav> */}
-
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/form" element={<FormPage />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-      </Routes>
-
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/form"
+            element={
+              <ProtectedRoute>
+                <FormPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/viewprofile" element={<ViewProfile />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
