45ad056b9b0573fb373396cf9c2705391640577d
Gandharv Kumar <87307496+Gandharv-999fx@users.noreply.github.com>
2025-11-12 21:10:08 +0530

    Update LandingPage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import HomePage from './HomePage'
 import AboutUs from './AboutUs'
 import Testimonials from '../components/testimonials'
 import FAQ from './FAQ'
+import Responsibility from './Responsibility'
 
 export const LandingPage = () => {
   return (
     <>
     <HomePage />
     <AboutUs />
+    <Responsibility />
     <Testimonials/>
     <FAQ />
     </>
