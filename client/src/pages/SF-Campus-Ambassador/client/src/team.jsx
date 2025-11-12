7589ffc45489beafd34bcb73d83b20a5a8e51ee6
Anuhya Chidurala <anuhya.chidurala1503@gmail.com>
2025-11-13 03:40:29 +0530

    Update team.jsx

--- client/src/pages/SF-Campus-Ambassador/client/src/team.jsx
+++ client/src/pages/SF-Campus-Ambassador/client/src/team.jsx
@@ -1,17 +1,1 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import OurTeam from "./pages/OurTeam.jsx";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/ourteam" element={<OurTeam />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
 
